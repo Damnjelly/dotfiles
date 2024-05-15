1c71c933baa1869d51ebc50ba60bffa7f179ba6e
Damnjelly <joren122@hotmail.com>
2024-05-15 12:39:29 +0200

    added dunst configuration

--- features/home-manager/niri/default.nix
+++ features/home-manager/niri/default.nix
@@ -1,9 +1,8 @@
 { pkgs, inputs, ... }: {
-  imports = [ inputs.niri.homeModules.niri ./niri.nix ./waybar.nix ./i3.nix ];
+  imports = [ inputs.niri.homeModules.niri ./niri.nix ./waybar.nix ./i3.nix ./dunst.nix ];
   home.packages = with pkgs; [
     blueman
     xwayland
-    dunst
     wl-clipboard # copy to clipboard
   ];
   stylix.targets = { gtk.enable = true; };
