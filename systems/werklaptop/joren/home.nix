b4d9f47d843cfb0b97a2c19a7b5c6b1f87dcc44f
Damnjelly <joren122@hotmail.com>
2024-05-24 17:10:38 +0000

    changes to werklaptop config

---
+++ systems/werklaptop/joren/home.nix
@@ -0,0 +1,16 @@
+{ lib, inputs, config, ... }: {
+  imports = [
+    inputs.impermanence.nixosModules.home-manager.impermanence
+    ./../../global-home.nix
+    ./../../../users/gelei.nix
+
+    ./../../../features/home-manager/cli
+    ./../../../features/home-manager/nixvim
+    ./../../../features/home-manager/shell
+  ];
+  config = {
+    theme = "madotsuki";
+
+    home.stateVersion = lib.mkDefault "23.11";
+  };
+}
