871c56fc79ee331ab4afb654cc1a294f65168f20
Damnjelly <joren122@hotmail.com>
2024-06-04 11:37:57 +0200

    added the shitty nvidia drivers again (they still suck)

--- systems/desktop/configuration.nix
+++ systems/desktop/configuration.nix
@@ -144,6 +144,17 @@
       gnome.gnome-keyring
     ];
   };
+# services.xserver.videoDrivers = ["nvidia"];
+
+# hardware.nvidia = {
+#   modesetting.enable = true;
+#   powerManagement.enable = false;
+#   powerManagement.finegrained = false;
+#   open = false;
+#   nvidiaSettings = true;
+#   package = config.boot.kernelPackages.nvidiaPackages.latest;
+# };
+
 
   # open ports for Kde connect
   networking = {
