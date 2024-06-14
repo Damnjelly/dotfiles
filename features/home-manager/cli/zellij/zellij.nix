9dbe2b3b8b32fea257f6a16e3f62b3b88fdc2d25
Damnjelly <joren122@hotmail.com>
2024-06-14 19:11:17 +0200

    removed stylix from zellij because autoenable

--- features/home-manager/cli/zellij/zellij.nix
+++ features/home-manager/cli/zellij/zellij.nix
@@ -2,7 +2,6 @@
   config = {
     home.file.".config/zellij/layouts/development.kdl".text =
       "${builtins.readFile ./development.kdl}";
-    stylix.targets.zellij.enable = true;
     programs.zellij = {
       enable = true;
       settings = {
