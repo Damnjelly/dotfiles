3eb3b78161437be0180852623d0cb876f9815162
Damnjelly <joren122@hotmail.com>
2024-06-14 19:11:32 +0200

    added padding

--- features/home-manager/cli/foot.nix
+++ features/home-manager/cli/foot.nix
@@ -1,4 +1,9 @@
-{ ... }: {
+{
   stylix.targets.foot.enable = true;
-  programs.foot = { enable = true; };
+  programs.foot = {
+    enable = true;
+    settings.main = {
+      pad = "6x6center";
+    };
+  };
 }
