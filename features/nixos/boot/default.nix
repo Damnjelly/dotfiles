de834141d46d4aa938a778c70d3fee41a5867639
Damnjelly <joren122@hotmail.com>
2024-06-19 21:51:03 +0200

    removed kmscon

--- features/nixos/boot/default.nix
+++ features/nixos/boot/default.nix
@@ -2,6 +2,5 @@
 {
   imports = [
     ./greetd.nix
-    # ./kmscon.nix
   ];
 }
--- features/nixos/system-packages.nix
+++ features/nixos/system-packages.nix
@@ -3,6 +3,7 @@
   environment.systemPackages = with pkgs; [
     git
     nh
+    sops
   ];
   fonts.packages = with pkgs; [
     kirsch
