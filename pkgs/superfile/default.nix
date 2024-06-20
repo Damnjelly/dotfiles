e50cfeeedeb552698deccd022a43f455bc64b0f6
Damnjelly <joren122@hotmail.com>
2024-06-20 19:54:45 +0200

    updated superfile

--- pkgs/superfile/default.nix
+++ pkgs/superfile/default.nix
@@ -14,11 +14,11 @@ buildGoModule {
     fetchFromGitHub {
       owner = "yorukot";
       repo = "superfile";
-      rev = "v${version}";
-      hash = "sha256-z1jcRzID20s7tEDUaEcnOYBfv/BPZtcXz9fy3V5iPPg=";
+      rev = "1a15d23ebbdf84fec98725b7635e6071e61163a2";
+      hash = "sha256-pgPdvdB9J7MA99yRgISyAS4RR7BPXe3WgzeG/F4xSLE=";
     };
 
-  vendorHash = "sha256-OzPH7dNu/V4HDGSxrvYxu3s+hw36NiulFZs0BJ44Pjk=";
+  vendorHash = "sha256-750y77tT2CtkuELyr6fDc7DjIWyYyTM4qDHx4lgvLmc=";
 
   meta = {
     changelog = "https://github.com/yorukot/superfile/blob/v${version}/changelog.md";
