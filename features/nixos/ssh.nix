07f421c990c9150339f288442ad7f18ac89b9d71
Damnjelly <joren122@hotmail.com>
2024-06-23 02:03:16 +0200

    fix typo

--- features/nixos/ssh.nix
+++ features/nixos/ssh.nix
@@ -4,7 +4,7 @@
       "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
       "[galaxy]:145" = {
         publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOXNK+8zCSAQM/OK52rfnIHY4zYhLfeAw0RNdInPMKr8";
-        extraHostNames = [ "[galaxy.wyvern-wahoo.ts.net]:145" "[192.168.1.116:145]:145" "[100.98.198.36]:145" ];
+        extraHostNames = [ "[galaxy.wyvern-wahoo.ts.net]:145" "[192.168.1.116]:145" "[100.98.198.36]:145" ];
       };
     };
   };
