feb060d9eb06218b59442a81e9ace1d4cb711fcf
Damnjelly <joren122@hotmail.com>
2024-07-11 20:16:19 +0200

    removed clematis, fragments and nyaa, added aberol instead of g4music

--- features/home-manager/music/default.nix
+++ features/home-manager/music/default.nix
@@ -1,20 +1,31 @@
-{ pkgs, inputs, ... }:
+{ pkgs, inputs, lib, config, ... }:
 {
-  imports = [ ./g4music.nix inputs.nyaa.homeManagerModule ];
-  home.packages = with pkgs; [
-    g4music
-    tagger
-    clematis
-    gnome.adwaita-icon-theme
-    fragments
+  imports = [
+    inputs.nyaa.homeManagerModule
   ];
-  programs.nyaa = {
-    enable = true;
-    scroll_padding = 6;
-    download_client = "RunCommand";
-    client.cmd = {
-      cmd = "fragments {torrent}";
-      shell_cmd = "sh -c";
+  home = {
+    packages = with pkgs; [
+      aberol
+      tagger
+      gnome.adwaita-icon-theme
+    ];
+    persistence = lib.mkIf config.optinpermanence.enable {
+      "/persist/home/${config.home.username}/music" = {
+        directories = [ ".cache/aberol" ];
+        allowOther = true;
+      };
+    };
+    sessionVariables = {
+      MUSIC = "aberol";
     };
   };
+  # programs.nyaa = {
+  #   enable = true;
+  #   scroll_padding = 6;
+  #   download_client = "RunCommand";
+  #   client.cmd = {
+  #     cmd = "fragments {torrent}";
+  #     shell_cmd = "sh -c";
+  #   };
+  # };
 }
