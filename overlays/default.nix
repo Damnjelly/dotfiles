ff53d63a005110d6715cdf7eb4133840c72c583b
Damnjelly <juinen@proton.me>
2024-07-16 23:38:16 +0200

    updated flakes

--- overlays/default.nix
+++ overlays/default.nix
@@ -19,13 +19,5 @@
     #     };
     #   });
   };
-  # When applied, the stable nixpkgs set (declared in the flake inputs) will
-  # be accessible through 'pkgs.stable'
-  stable-packages = final: _prev: {
-    stable = import inputs.nixpkgs-stable {
-      system = final.system;
-      config.allowUnfree = true;
-    };
-  };
   mons-package = final: prev: { monspkgs = import inputs.nixpkgs-mons { system = final.system; }; };
 }
