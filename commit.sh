2ec783250a28dcac3d59ce68e8f2a52d023eb62a
Damnjelly <juinen@proton.me>
2024-07-16 23:38:53 +0200

    why was this still in here

--- commit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ -z "$(git status -s -uno | grep -v '^ ' | awk '{print $2}')" ]; then
-	gum confirm "Stage all?" && git add .
-fi
-
-GITMES=$(gum input --placeholder "commit message")
-git commit -m "$GITMES"
-
-git push
