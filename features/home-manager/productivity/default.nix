359e4a0e6f8ffd4d558dd9f803a5b2309be1e678
Damnjelly <juinen@proton.me>
2024-09-06 14:23:04 +0200

    lowercased persistence

--- features/home-manager/productivity/default.nix
+++ features/home-manager/productivity/default.nix
@@ -10,9 +10,16 @@
     remmina
     citrix_workspace
     jetbrains.idea-community
+    onlyoffice-bin_latest
+
+    corefonts
+    inter
   ];
+
+  fonts.fontconfig.enable = true;
+
   home.persistence = lib.mkIf config.optinpermanence.enable {
-    "/persist/home/${config.home.username}/Productivity" = {
+    "/persist/home/${config.home.username}/productivity" = {
       directories = [
         ".config/teams-for-linux"
         ".thunderbird"
