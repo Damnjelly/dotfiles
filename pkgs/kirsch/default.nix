b492bab1da68564a5ac0b5a570de1ed60bbcd480
Damnjelly <juinen@proton.me>
2024-12-03 10:39:15 +0100

    updated kirsch

--- pkgs/kirsch/default.nix
+++ pkgs/kirsch/default.nix
@@ -1,19 +1,20 @@
 { lib, fetchzip }:
 let
   pname = "kirsch";
-  version = "v0.0.8";
+  version = "v0.0.11";
 in
 fetchzip {
   name = "${pname}-${version}";
 
   url = "https://github.com/molarmanful/kirsch/releases/download/${version}/kirsch_${version}.zip";
-  sha256 = "sha256-HCVSmkBlwmcoI8YV4YUpsY94IdCaE60pdvXob4y5X1M=";
+  sha256 = "sha256-mH3FTlrdFAmkovqawF5OWgdas+hHJnVFhrUzXoOUBtc=";
 
   postFetch = ''
     mkdir -p $out/share/fonts/misc
     mv $out/*.otb $out/share/fonts/misc
     mv $out/*.pcf $out/share/fonts/misc
     mv $out/*.dfont $out/share/fonts/misc
+    mv $out/*.bdf $out/share/fonts/misc
 
     mkdir -p $out/share/fonts/woff2
     mv $out/*.woff2 $out/share/fonts/woff2
